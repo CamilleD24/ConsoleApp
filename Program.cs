b71d6af113a9711ee5341f51258276ecf3ed7525
Jerem <Jerem@LAPTOP-FA72QV8P>
2025-03-24 11:40:59 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Square(int INTa, float b, float FLOATc, float n)
+    {
+        b = INTa / 12;
+        Console.WriteLine("Par mois le salaire brut est de " + b + "euros");
+        n = b * FLOATc;
+        Console.WriteLine("Le salaire net est de " + n + "euros");
+
+
+    }
+    static void Main(string[] args)
+    {
+        string a = args[0];
+        string c = args[1];
+        int INTa = int.Parse(a);
+        float FLOATc = float.Parse(c);
+        float b = 0;
+        float n = 0;
+        Square(INTa, FLOATc, b, n);
+        Console.WriteLine("En sachant que le salaire brut annuel est de " + a + "euros");
+    }
+
+}
+
